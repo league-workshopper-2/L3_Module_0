23761df6de42b3e72333011e84850402c140f4c1
TheSirLancelot <league@WTS-IM-6.attlocal.net>
2017-10-18 18:28:52 -0700

    Stacks Module 3

--- src/IntroToStacks/IntroToStack.java
+++ src/IntroToStacks/IntroToStack.java
@@ -1,7 +1,10 @@
 package IntroToStacks;
 
 import java.awt.geom.Arc2D.Double;
+import java.util.Random;
 import java.util.Stack;
+
+import javax.swing.JOptionPane;
 
 public class IntroToStack {
 	public static void main(String[] args) {
@@ -9,11 +12,18 @@ public class IntroToStack {
 		//   Don't forget to import the Stack class
 		Stack<Double> doubles = new Stack<Double>();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		for (int i = 0; i < doubles; i++) {
-			
+		Random rand=new Random();
+		Double dubs;
+		for (int i = 0; i < 100; i++) {
+		dubs=rand.nextDouble();	
+		System.out.println(dubs);
+		doubles.push(dubs);
 		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+		String nums =JOptionPane.showInputDialog("Type a number between 0 and 100");
+		int numbers=Integer.parseInt(nums);
+		String nums2 = JOptionPane.showInputDialog("Type another number between 0 and 100");
+		int numbers2=Integer.parseInt(nums2);
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
 		
