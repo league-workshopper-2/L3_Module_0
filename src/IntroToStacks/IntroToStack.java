4cef14d13dae00d98d6485fbac51d8d8e7597814
TJHL <league@WTS-IM-6.attlocal.net>
2017-10-25 18:26:52 -0700

    Started TextUndoRedo l3 Module 0

--- src/IntroToStacks/IntroToStack.java
+++ src/IntroToStacks/IntroToStack.java
@@ -1,6 +1,4 @@
 package IntroToStacks;
-
-import java.awt.geom.Arc2D.Double;
 import java.util.Random;
 import java.util.Stack;
 
@@ -13,11 +11,11 @@ public class IntroToStack {
 		Stack<Double> doubles = new Stack<Double>();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
 		Random rand=new Random();
-		Double dubs;
+		double random=0;
 		for (int i = 0; i < 100; i++) {
-		dubs=rand.nextDouble();	
-		System.out.println(dubs);
-		doubles.push(dubs);
+		random = rand.nextDouble()*100;	
+		System.out.println(random);
+		doubles.push(random);
 		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
 		String nums =JOptionPane.showInputDialog("Type a number between 0 and 100");
@@ -27,6 +25,13 @@ public class IntroToStack {
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
 		
+	System.out.println("Here is a list of doubles between " + numbers + " and "+ numbers2);
+	for (int i = 0; i < 100; i++) {
+	double d = doubles.pop();
+		if (d > numbers && d < numbers2) {
+			System.out.println(d);
+		}
+	}
 		//   EXAMPLE:
 		//   NUM 1: 65
 		//   NUM 2: 75
--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -1,6 +1,30 @@
 package IntroToStacks;
 
-public class TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TextUndoRedo implements KeyListener {
+	
+	public static void main(String[] args) {
+		TextUndoRedo redo = new TextUndoRedo();
+		redo.stuff();
+	}
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	public void stuff(){
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setSize(500, 500);
+		panel.add(label);
+		label.setVisible(true);
+		label.addKeyListener(this);
+	}
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,5 +37,23 @@ public class TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		String keys;
+		keys=e.getKeyText(0);
+		label.setText(keys);
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
